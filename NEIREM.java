8d6b99bcc9da2309a56e234d2b94164b06fe43b8
Tanu Shree Verma <142032256+tanushree1427@users.noreply.github.com>
2024-04-26 00:36:46 +0530

    Add files via upload
    https://www.codechef.com/problems/NEIREM

---
+++ NEIREM.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n;
+		Scanner sc= new Scanner(System.in);
+		 n=sc.nextInt();
+		System.out.println(+100-n);
+
+	}
+}
