b6b3e8a994919f586945f8025f892712d121c162
Tanu Shree Verma <142032256+tanushree1427@users.noreply.github.com>
2024-04-26 00:22:01 +0530

    Add files via upload
    https://www.codechef.com/problems/GIANT?tab=statement

---
+++ GIANT.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    	int x;
+			Scanner sc=new Scanner(System.in);
+	     	x=sc.nextInt();
+	
+	   	if(x >= 60)
+		{
+		    System.out.println("yes\n");
+		}
+		else
+		{
+		     System.out.println("No\n");
+		}
+	
+		
+
+	}
+}
